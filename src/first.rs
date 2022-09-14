d06cd7ad0adf6058ae4de47722566086c8f66bc2
huchenxu <huchenxu@stargraph.cn>
2022-09-14 23:54:59 +0800

    build a stack

---
+++ src/first.rs
@@ -0,0 +1,59 @@
+
+pub struct  List{
+    header: Link,
+}
+#[derive(Clone)]
+enum Link {
+    Empty,
+    More(Box<Node>)
+}
+#[derive(Clone)]
+pub struct Node{
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new()->Self{
+        // build a list, header points to Link::Empty, 
+        List { header: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32){
+        // new a Node struct at heap, node.next points to self.header,
+        // put node value into Link::More()
+        // change points of self.headr to Link Type entity 
+        let new_node = Box::new(Node{
+            elem: elem,
+            next: self.header.clone(),
+        });
+        self.header = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32>{
+        //get header, change the mem, change self.header to the previous element
+        match std::mem::replace(&mut self.header, Link::Empty){
+            Link::Empty => None,
+            Link::More(x) => {
+                self.header = x.next;
+                Some(x.elem)
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use super::List;
+    #[test]
+    fn basic_test(){
+        let mut  list = List::new();
+        list.push(1);
+        list.push(2);
+        list.pop();
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(),Some(1));
+    }
+}
